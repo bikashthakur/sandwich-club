1c1da998b4e198330a8b40eaf73062082bdbda27
Bikash Thakur <thakurbikash183@gmail.com>
2018-06-23 01:49:09 +0530

    updated JsonUtils with constant JSON keys

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -13,45 +13,54 @@ import java.util.List;
 
 public class JsonUtils {
 
+    private static final String JSON_KEY_NAME = "name";
+    private static final String JSON_KEY_MAIN_NAME = "mainName";
+    private static final String JSON_KEY_ALSO_KNOWN_AS = "alsoKnownAs";
+    private static final String JSON_KEY_PLACE_OF_ORIGIN = "placeOfOrigin";
+    private static final String JSON_KEY_DESCRIPTION = "description";
+    private static final String JSON_KEY_IMAGE_URL = "image";
+    private static final String JSON_KEY_INGREDIENTS = "ingredients";
+
+    private static final String JSON_DEFAULT_DATA_TEXT = "Not Available";
+
     public static Sandwich parseSandwichJson(String json) {
 
         JSONObject sandwichJSON;
-        String defaultText = "Not Available";
 
         try {
+
             sandwichJSON = new JSONObject(json);
 
-            JSONObject sandwichName = sandwichJSON.getJSONObject("name");
+            if (sandwichJSON.has(JSON_KEY_NAME)) {
 
-            String mainName = sandwichName.getString("mainName").isEmpty() ? defaultText : sandwichName.getString("mainName");
+                JSONObject sandwichName = sandwichJSON.getJSONObject(JSON_KEY_NAME);
+                String mainName = sandwichName.optString(JSON_KEY_MAIN_NAME, JSON_DEFAULT_DATA_TEXT);
+                JSONArray alsoKnownAs = sandwichName.getJSONArray(JSON_KEY_ALSO_KNOWN_AS);
+                List<String> otherNames = new ArrayList<>();
 
-            JSONArray alsoKnownAs = sandwichName.getJSONArray("alsoKnownAs");
+                if (alsoKnownAs.length() != 0) {
+                    otherNames = convertJSONArrayToArrayList(alsoKnownAs);
+                } else {
+                    otherNames.add(JSON_DEFAULT_DATA_TEXT);
+                }
 
-            List<String> otherNames = new ArrayList<>();
+                String origin = sandwichJSON.optString(JSON_KEY_PLACE_OF_ORIGIN, JSON_DEFAULT_DATA_TEXT);
+                String desc = sandwichJSON.optString(JSON_KEY_DESCRIPTION, JSON_DEFAULT_DATA_TEXT);
 
-            if (alsoKnownAs.length() != 0) {
-                otherNames = convertJSONArrayToArrayList(alsoKnownAs);
-            } else {
-                otherNames.add(defaultText);
+                String imgURL = sandwichJSON.has(JSON_KEY_IMAGE_URL) ? sandwichJSON.optString(JSON_KEY_IMAGE_URL) : null;
+
+                JSONArray ingredientsJSONArray = sandwichJSON.getJSONArray(JSON_KEY_INGREDIENTS);
+                List<String> inggredients = new ArrayList<>();
+
+                if (ingredientsJSONArray.length() != 0) {
+                    inggredients = convertJSONArrayToArrayList(ingredientsJSONArray);
+                } else {
+                    inggredients.add(JSON_DEFAULT_DATA_TEXT);
+                }
+
+                return new Sandwich(mainName, otherNames, origin, desc, imgURL, inggredients);
+
             }
-
-            String origin = sandwichJSON.getString("placeOfOrigin").isEmpty() ? defaultText : sandwichJSON.getString("placeOfOrigin");
-
-            String desc = sandwichJSON.getString("description").isEmpty() ? defaultText : sandwichJSON.getString("description");
-
-            String imgURL = sandwichJSON.getString("image");
-
-            JSONArray ingredientsJSONArray = sandwichJSON.getJSONArray("ingredients");
-
-            List<String> inggredients = new ArrayList<>();
-
-            if (ingredientsJSONArray.length() != 0) {
-                inggredients = convertJSONArrayToArrayList(ingredientsJSONArray);
-            } else {
-                inggredients.add(defaultText);
-            }
-
-            return new Sandwich(mainName, otherNames, origin, desc, imgURL, inggredients);
 
         } catch (JSONException e) {
             Log.d("parseSandwichJson", "Error in parsing json data: " + e.getMessage());
@@ -65,7 +74,7 @@ public class JsonUtils {
         ArrayList<String> result = new ArrayList<>();
 
         for (int i = 0; i < data.length(); i++) {
-            result.add(data.getString(i));
+            result.add(data.optString(i));
         }
 
         return result;
